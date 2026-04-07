4dbecc9bb3cc6c53cb56a101ecfedc7f6cea8a46
Shivangi Asthana <shivangiasthana1862@gmail.com>
2026-04-07 17:53:29 +0530

    Update Main.java

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -16,10 +16,11 @@ public class Main {
             }
         }
         return sum == num;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
+       for (int i = 0; i < n; i++) {
             int num = sc.nextInt();
             boolean prime = isPrime(num);
             boolean perfect = isPerfect(num);
