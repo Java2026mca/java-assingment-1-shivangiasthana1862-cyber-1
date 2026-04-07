1989cae42636e05d3b519743fb836d429dca2b89
Shivangi Asthana <shivangiasthana1862@gmail.com>
2026-04-07 18:27:39 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            int num = 1; 
+            int num = 1; // First value in every row is always 1
             for (int j = 0; j <= i; j++) {
                 System.out.print(num);
                 if (j < i) {
