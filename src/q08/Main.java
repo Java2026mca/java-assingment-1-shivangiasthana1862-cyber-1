d36ea247b472031311dc0830bc20a3ebad4d3cb3
Shivangi Asthana <shivangiasthana1862@gmail.com>
2026-04-07 18:36:23 +0530

    Update Main.java

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -1,16 +1,21 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] stack = new int[n]; 
+
+        int[] stack = new int[n]; // max size = n operations
         int top = -1;
+
         for (int i = 0; i < n; i++) {
             String op = sc.next();
+
             if (op.equals("PUSH")) {
                 int x = sc.nextInt();
                 top++;
                 stack[top] = x;
+
             } else if (op.equals("POP")) {
                 if (top == -1) {
                     System.out.println("EMPTY");
@@ -18,12 +23,14 @@ public class Main {
                     System.out.println(stack[top]);
                     top--;
                 }
+
             } else if (op.equals("PEEK")) {
                 if (top == -1) {
                     System.out.println("EMPTY");
                 } else {
                     System.out.println(stack[top]);
                 }
+
             } else if (op.equals("SIZE")) {
                 System.out.println(top + 1);
             }
