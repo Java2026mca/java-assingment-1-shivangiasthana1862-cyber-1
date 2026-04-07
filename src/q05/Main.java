257d598b5fd15e7d6117ce3c097e2308d4d2081d
Shivangi Asthana <shivangiasthana1862@gmail.com>
2026-04-07 18:33:02 +0530

    Update Main.java

--- src/q05/Main.java
+++ src/q05/Main.java
@@ -1,23 +1,31 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String shape = sc.next();
+
         double area = 0;
+
         if (shape.equalsIgnoreCase("circle")) {
             double r = sc.nextDouble();
             area = Math.PI * r * r;
+
         } else if (shape.equalsIgnoreCase("rectangle")) {
             double l = sc.nextDouble();
             double w = sc.nextDouble();
             area = l * w;
+
         } else if (shape.equalsIgnoreCase("triangle")) {
             double a = sc.nextDouble();
             double b = sc.nextDouble();
             double c = sc.nextDouble();
+
             double s = (a + b + c) / 2.0;
             area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
         }
+
+        // Print with exactly 2 decimal places
         System.out.printf("Area: %.2f", area);
     }
 }
