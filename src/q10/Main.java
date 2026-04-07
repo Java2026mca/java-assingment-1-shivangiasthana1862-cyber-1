a85532655effda42966eb7931e6335fcec40b496
Shivangi Asthana <shivangiasthana1862@gmail.com>
2026-04-07 18:38:25 +0530

    Update Main.java

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -1,18 +1,27 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
+
         Stack<Integer> stack = new Stack<>();
+
         String[] tokens = line.split(" ");
+
         for (String token : tokens) {
+
+            // If token is a number
             if (Character.isDigit(token.charAt(0))) {
                 stack.push(Integer.parseInt(token));
             } 
+            // If token is an operator
             else {
                 int b = stack.pop();
                 int a = stack.pop();
+
                 int result = 0;
+
                 switch (token) {
                     case "+":
                         result = a + b;
@@ -27,9 +36,12 @@ public class Main {
                         result = a / b;
                         break;
                 }
+
                 stack.push(result);
             }
         }
+
+        // Final result
         System.out.println(stack.pop());
     }
 }
